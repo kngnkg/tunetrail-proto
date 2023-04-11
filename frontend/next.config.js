297fb5c462251c2e7ec5aef42c082423efa10f63
ryokawa <kawata.ryo.0518@gmail.com>
2023-04-11 10:50:48 +0000

    add storybook

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  compiler: (() => {
+    let compilerConfig = {};
 
-module.exports = nextConfig
+    if (process.env.NODE_ENV === 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+        // 本番環境ではReact Testing Libraryで使用するdata-testid属性を削除
+        reactRemoveProperties: { properties: ['^data-testid$'] },
+      };
+    }
+
+    return compilerConfig;
+  })(),
+};
+
+module.exports = nextConfig;
--- frontend/src/theme.ts
+++ frontend/src/theme.ts
@@ -2,6 +2,7 @@ import { Roboto } from 'next/font/google';
 import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
 
+// font
 export const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
   subsets: ['latin'],
