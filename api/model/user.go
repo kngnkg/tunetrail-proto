3a18ece6c91a8120ab681da98751563937005e6d
ryokawa <kawata.ryo.0518@gmail.com>
2023-04-18 13:11:55 +0000

    Add: validation

--- api/model/user.go
+++ api/model/user.go
@@ -1,6 +1,12 @@
 package model
 
-import "time"
+import (
+	"regexp"
+	"time"
+	"unicode"
+
+	"github.com/go-playground/validator/v10"
+)
 
 type User struct {
 	Id        int       `json:"id" db:"id"`
@@ -15,3 +21,48 @@ type User struct {
 }
 
 type Users []User
+
+// UserRegisterRequestはユーザー登録時のリクエストをバインドする構造体
+type UserRegisterRequest struct {
+	UserName string `json:"userName" binding:"required,min=3,max=20"`
+	Name     string `json:"name" binding:"required,min=3,max=20"`
+	Password string `json:"password" binding:"required,password"`
+	Email    string `json:"email" binding:"required,email"`
+	IconUrl  string `json:"iconUrl" binding:"required,url"`
+	Bio      string `json:"bio" binding:"required,max=1000"`
+}
+
+// PasswordValidationFunctionはパスワードのバリデーションを行う関数
+// TODO: テストを書く
+func PasswordValidationFunction(fl validator.FieldLevel) bool {
+	password := fl.Field().String()
+
+	// 8文字以上20文字以下である
+	if len(password) < 8 || len(password) > 20 {
+		return false
+	}
+
+	// 英数字のみである
+	alphanumeric := regexp.MustCompile("^[a-zA-Z0-9]*$")
+	if !alphanumeric.MatchString(password) {
+		return false
+	}
+
+	hasUpper, hasLower, hasNumber := false, false, false
+
+	for _, r := range password {
+		switch {
+		case unicode.IsUpper(r):
+			hasUpper = true
+		case unicode.IsLower(r):
+			hasLower = true
+		case unicode.IsNumber(r):
+			hasNumber = true
+		}
+	}
+
+	// 1文字以上の大文字が含まれている
+	// 1文字以上の小文字が含まれている
+	// 1文字以上の数字が含まれている
+	return hasUpper && hasLower && hasNumber
+}
