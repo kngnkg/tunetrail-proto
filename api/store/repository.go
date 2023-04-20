681eb8848827a2162488c71a7270f1c09faf0b80
ryokawa <kawata.ryo.0518@gmail.com>
2023-04-20 12:27:59 +0000

    Fix: error message

--- api/store/repository.go
+++ api/store/repository.go
@@ -21,19 +21,19 @@ const (
 // storeパッケージで用いるエラー
 var (
 	// DBとの疎通が取れない
-	ErrCannotCommunicateWithDB = errors.New("cannot communicate with db")
+	ErrCannotCommunicateWithDB = errors.New("store: cannot communicate with db")
 	// トランザクション開始に失敗
-	ErrBeginTxFailed = errors.New("begin tx failed")
+	ErrBeginTxFailed = errors.New("store: begin tx failed")
 	// ロールバックに失敗
-	ErrRollbackFailed = errors.New("rollback failed")
+	ErrRollbackFailed = errors.New("store: rollback failed")
 	// コミットに失敗
-	ErrCommitFailed = errors.New("commit failed")
+	ErrCommitFailed = errors.New("store: commit failed")
 	// ユーザーが見つからない
-	ErrUserNotFound = errors.New("user not found")
+	ErrUserNotFound = errors.New("store: user not found")
 	// ユーザー名が既に存在する
-	ErrUserNameAlreadyExists = errors.New("user name already exists")
+	ErrUserNameAlreadyExists = errors.New("store: user name already exists")
 	// メールアドレスが既に存在する
-	ErrEmailAlreadyExists = errors.New("email already exists")
+	ErrEmailAlreadyExists = errors.New("store: email already exists")
 )
 
 type Repository struct {
