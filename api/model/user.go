ff4b8e334a415be83eb2cc33b4f8cb822496ba27
ryokawa <kawata.ryo.0518@gmail.com>
2023-05-01 02:42:24 +0000

    Add: validation to User struct

--- api/model/user.go
+++ api/model/user.go
@@ -5,13 +5,13 @@ import (
 )
 
 type User struct {
-	Id        int       `json:"id" db:"id"`
-	UserName  string    `json:"userName" db:"user_name"`
-	Name      string    `json:"name" db:"name"`
-	Password  string    `json:"password" db:"password"`
-	Email     string    `json:"email" db:"email"`
-	IconUrl   string    `json:"iconUrl" db:"icon_url"`
-	Bio       string    `json:"bio" db:"bio"`
+	Id        int       `json:"id" db:"id" binding:"required"`
+	UserName  string    `json:"userName" db:"user_name" binding:"required,min=3,max=20"`
+	Name      string    `json:"name" db:"name" binding:"required,min=3,max=20"`
+	Password  string    `json:"password" db:"password" binding:"required,password"`
+	Email     string    `json:"email" db:"email" binding:"required,email"`
+	IconUrl   string    `json:"iconUrl" db:"icon_url" binding:"required,url"`
+	Bio       string    `json:"bio" db:"bio" binding:"required,max=1000"`
 	CreatedAt time.Time `json:"createdAt" db:"created_at"`
 	UpdatedAt time.Time `json:"updatedAt" db:"updated_at"`
 }
