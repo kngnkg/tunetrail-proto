2c0983a4852f93421519bc2ebadcac1f13db20b2
ryokawa <kawata.ryo.0518@gmail.com>
2023-05-16 09:03:39 +0900

    Add: ACM certificate settings

--- infra/alb.tf
+++ infra/alb.tf
@@ -1,20 +1,41 @@
-resource "aws_lb" "alb" {
+# Application Load Balancer (ALB)
+resource "aws_lb" "tunetrail" {
   name               = "tunetrail-alb"
-  internal           = false # 内部向けのALBではなく、インターネット向けのALB
+  internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = [aws_subnet.public1.id, aws_subnet.public2.id]
+  subnets            = [aws_subnet.public1.id, aws_subnet.public2.id] # ALBを配置するパブリックサブネット
 }
 
-# ALBのリスナー
-resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.alb.arn
+# HTTPリスナー
+# HTTPでの接続はHTTPSへリダイレクトする
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.tunetrail.arn
   port              = "80"
   protocol          = "HTTP"
 
-  # デフォルトアクション（リクエストが他のルールにマッチしない場合に適用される）
   default_action {
-    type             = "forward"                      # リクエストをフォワード
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+# HTTPSリスナー
+# ACMで発行した証明書を使用して、HTTPSでの接続を受け付ける
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.tunetrail.arn # 上記で作成したALBを指定
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"       # デフォルトのセキュリティポリシーを指定
+  certificate_arn   = aws_acm_certificate.tunetrail.arn # ACMで発行した証明書のARNを指定
+
+  default_action {
+    type             = "forward"
     target_group_arn = aws_lb_target_group.alb_tg.arn # フォワード先のターゲットグループ
   }
 }
@@ -24,18 +45,26 @@ resource "aws_lb_target_group" "alb_tg" {
   name     = "tunetrail-alb-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+  vpc_id   = aws_vpc.main.id # VPCを指定
 }
 
 # ALBに適用するセキュリティグループ
 resource "aws_security_group" "alb_sg" {
-  name        = "allow_http"
-  description = "Allow HTTP inbound traffic"
+  name        = "allow_http_https"
+  description = "Allow HTTP and HTTPS inbound traffic"
 
-  # インバウンドルールの設定
+  # HTTPに対するインバウンドルール
   ingress {
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # 任意のIPからのアクセスを許可
+  }
+
+  # HTTPSに対するインバウンドルール
+  ingress {
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"] # 任意のIPからのアクセスを許可
   }
--- infra/route53.tf
+++ infra/route53.tf
@@ -3,6 +3,45 @@ resource "aws_route53_zone" "tunetrail" {
   name = "tune-trail.com"
 }
 
+# ACM証明書
+resource "aws_acm_certificate" "tunetrail" {
+  domain_name               = "*.tune-trail.com"
+  subject_alternative_names = ["tune-trail.com"]
+  validation_method         = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# CNAMEレコード
+resource "aws_route53_record" "cert_validation" {
+  # 複数のドメインに対する検証レコードを作成する
+  # 各ドメイン名をキーとし、対応するレコード情報を値とするマップを作成
+  for_each = {
+    for dvo in aws_acm_certificate.tunetrail.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true # 既存のレコードを上書きする
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.tunetrail.zone_id
+}
+
+# ACM証明書のドメイン検証
+# 検証が完了するまで待機する
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn = aws_acm_certificate.tunetrail.arn
+  # aws_route53_record.cert_validation の全てのインスタンスを取得し
+  # そのfqdn属性をリストとして取得
+  validation_record_fqdns = values(aws_route53_record.cert_validation)[*].fqdn
+}
+
 # ALBへのエイリアスレコード (www)
 resource "aws_route53_record" "www" {
   zone_id = aws_route53_zone.tunetrail.zone_id
