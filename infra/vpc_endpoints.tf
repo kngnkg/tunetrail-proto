2e55fc1e3f6ad2b6dc7a4b0772b6b4ad94b02bb8
ryokawa <kawata.ryo.0518@gmail.com>
2023-05-22 11:41:26 +0900

    Fix: add vpc endpoint for cloudwatch logs

--- infra/vpc_endpoints.tf
+++ infra/vpc_endpoints.tf
@@ -27,3 +27,14 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_endpoint_type = "Gateway"
   route_table_ids   = [aws_route_table.private.id]
 }
+
+# CloudWatch Logs用のVPCエンドポイント
+resource "aws_vpc_endpoint" "cloudwatch_logs" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.ap-northeast-1.logs"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = [aws_subnet.private1.id, aws_subnet.private2.id]
+  security_group_ids  = [aws_security_group.sg.id]
+  private_dns_enabled = true
+}
+
