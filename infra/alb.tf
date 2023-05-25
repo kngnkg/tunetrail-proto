fa9b6645aaee874ef935860ae558c38feefc8419
ryokawa <kawata.ryo.0518@gmail.com>
2023-05-25 16:42:01 +0900

    Fix: change target type

--- infra/alb.tf
+++ infra/alb.tf
@@ -70,10 +70,11 @@ resource "aws_lb_target_group" "alb_tg" {
 
 # API用のターゲットグループ
 resource "aws_lb_target_group" "alb_tg_api" {
-  name     = "api-target-group"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id # VPCを指定
+  name        = "api-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id # VPCを指定
+  target_type = "ip"
 
   health_check {
     enabled             = true
