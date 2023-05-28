4f35bd81876f4ebdd62e5f660477e7b33de80b7e
ryokawa <kawata.ryo.0518@gmail.com>
2023-05-28 22:18:42 +0000

    Add: debug log

--- frontend/lib/getHealth.ts
+++ frontend/lib/getHealth.ts
@@ -4,10 +4,12 @@ import { HealthSchema } from "./validations/health.schema"
 
 // getHealthはAPIの稼働状態を取得する
 export const getHealth = async (context: ApiContext): Promise<Health> => {
+  console.log(`fetching ${context.apiRoot}/health`)
   const response = await fetch(`${context.apiRoot}/health`, {
     cache: "no-store",
   })
   if (!response.ok) {
+    console.error(response)
     const errorResponse = await response.json()
     const error = new Error(
       errorResponse.message ?? "Failed to fetch data from the API."
@@ -17,6 +19,8 @@ export const getHealth = async (context: ApiContext): Promise<Health> => {
 
   const data = await response.json()
   if (!data) {
+    console.error(`empty response from ${context.apiRoot}/health`)
+    console.error(response)
     throw new Error("Failed to fetch data from the API.")
   }
 
