c169d2d870203751a7bbac1490c161fed5484ad3
ryokawa <kawata.ryo.0518@gmail.com>
2023-05-29 23:03:10 +0000

    Fix: Redesigned health check page

--- frontend/app/health/page.tsx
+++ frontend/app/health/page.tsx
@@ -1,17 +1,10 @@
 import React from "react"
 
-import { apiContext } from "@/lib/apiContext"
-import { getHealth } from "@/lib/getHealth"
-import HealthStatus from "@/components/organisms/HealthStatus/HealthStatus"
-
 // ヘルスチェックの結果を表示するページ
 export default async function Health() {
-  const health = await getHealth(apiContext)
-
   return (
     <div className="container mx-auto p-8">
       <h1 className="text-3xl mb-8">Health Check Page</h1>
-      <HealthStatus health={health} />
     </div>
   )
 }
