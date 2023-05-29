2dba2fd03be68d075c70f81cbf1d3ee3faba80e0
ryokawa <kawata.ryo.0518@gmail.com>
2023-05-29 04:51:29 +0000

    Fix: Added error handling to getHealth

--- frontend/lib/getHealth.ts
+++ frontend/lib/getHealth.ts
@@ -4,30 +4,42 @@ import { HealthSchema } from "./validations/health.schema"
 
 // getHealthはAPIの稼働状態を取得する
 export const getHealth = async (context: ApiContext): Promise<Health> => {
-  console.log(`fetching ${context.serverApiRoot}/health`)
-  const response = await fetch(`${context.serverApiRoot}/health`, {
-    cache: "no-store",
-  })
-  if (!response.ok) {
-    console.error(response)
-    const errorResponse = await response.json()
-    const error = new Error(
-      errorResponse.message ?? "Failed to fetch data from the API."
-    )
-    throw error
-  }
+  console.log(`fetching ${context.apiRoot}/health`)
+  try {
+    const response = await fetch(`${context.apiRoot}/health`, {
+      cache: "no-store",
+    })
+    if (!response.ok) {
+      console.error(response)
+      const errorResponse = await response.json()
+      const error = new Error(
+        errorResponse.message ??
+          `Failed to fetch data from the API. Status: ${response.status}`
+      )
+      throw error
+    }
 
-  const data = await response.json()
-  if (!data) {
-    console.error(`empty response from ${context.serverApiRoot}/health`)
-    console.error(response)
-    throw new Error("Failed to fetch data from the API.")
-  }
+    const data = await response.json()
+    if (!data) {
+      throw new Error("data is empty from the API.")
+    }
 
-  const validationResult = HealthSchema.safeParse(data)
-  if (!validationResult.success) {
-    throw new Error(validationResult.error.message)
-  }
+    const validationResult = HealthSchema.safeParse(data)
+    if (!validationResult.success) {
+      throw new Error(validationResult.error.message)
+    }
 
-  return validationResult.data
+    return validationResult.data
+  } catch (e) {
+    if (e instanceof Error) {
+      console.error(e.message)
+    } else {
+      console.error(e)
+    }
+
+    return {
+      health: "red",
+      database: "red",
+    }
+  }
 }
