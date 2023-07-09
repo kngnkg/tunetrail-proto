185393110f5b23daf07f0480a9570d2deabbf453
ryokawa <kawata.ryo.0518@gmail.com>
2023-07-09 04:05:02 +0000

    Add: config package

---
+++ database/config/config.test.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNew(t *testing.T) {
+	wantPort := 9999
+	t.Setenv("DBPORT", fmt.Sprint(wantPort))
+
+	got, err := New()
+	if err != nil {
+		t.Fatalf("cannot create config: %v", err)
+	}
+	assert.Equal(t, wantPort, got.DBPort)
+	wantEnv := "dev"
+	assert.Equal(t, wantEnv, got.Env)
+}
