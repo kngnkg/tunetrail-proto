60a53a9fd28ed72bae7da92b862a95cb2dde9ab4
kngnkg <kawata.ryo.0518@gmail.com>
2023-08-13 09:20:49 +0000

    Add: signedin user context

---
+++ webapp/types/user.ts
@@ -0,0 +1,20 @@
+export type User = {
+  id: string
+  userName: string
+  name: string
+  iconUrl: string
+  bio: string
+  createdAt: string
+  updatedAt: string
+}
+export const isUser = (arg: any): arg is User => {
+  return (
+    arg.id !== undefined &&
+    arg.userName !== undefined &&
+    arg.name !== undefined &&
+    arg.iconUrl !== undefined &&
+    arg.bio !== undefined &&
+    arg.createdAt !== undefined &&
+    arg.updatedAt !== undefined
+  )
+}
