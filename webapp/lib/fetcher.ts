1967df8d1e0e0bed86514df9ddb5e104be5f3bd0
kngnkg <kawata.ryo.0518@gmail.com>
2023-08-14 05:18:02 +0000

    Fix: fetcher error handling

--- webapp/lib/fetcher.ts
+++ webapp/lib/fetcher.ts
@@ -1,3 +1,6 @@
+import { refreshAccessToken } from "@/services/auth/refreshAccessToken"
+
+import { env } from "@/env.mjs"
 import { isApiError } from "@/types/error"
 
 export const fetcher = async (
@@ -10,15 +13,29 @@ export const fetcher = async (
     if (!response.ok) {
       const errResp = await response.json()
 
-      if (isApiError(errResp)) {
-        // TODO: エラーコードによる分岐等
-        throw new Error(errResp.userMessage)
+      if (!isApiError(errResp)) {
+        // エラーレスポンスがAPIエラーでない場合はエラーを投げる
+        throw new Error(
+          errResp.message ??
+            `Failed to fetch data from the API. Status: ${response.status}`
+        )
       }
 
-      throw new Error(
-        errResp.message ??
-          `Failed to fetch data from the API. Status: ${response.status}`
-      )
+      // 認証エラーの場合
+      if (response.status === 401) {
+        switch (errResp.code) {
+          case 4105:
+            // トークンが期限切れの場合はリフレッシュトークンを送信して再度リクエストを送る
+            await refreshAccessToken(env.NEXT_PUBLIC_API_ROOT)
+            return fetcher(resource, init)
+          // case 4106:
+          // リフレッシュトークンが期限切れの場合はログイン画面に遷移する
+          // case 4107:
+          // CSRFトークンが期限切れの場合は...
+        }
+      }
+
+      throw new Error(errResp.userMessage)
     }
 
     return response.json()
