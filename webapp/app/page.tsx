a49cb7f8da89ac2355bd1283ac40c00390b6286e
kngnkg <kawata.ryo.0518@gmail.com>
2023-08-19 07:42:37 +0000

    Add: routing

--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -1,8 +1,6 @@
+import { redirect } from "next/navigation"
+
 // タイムライン
 export default function Home() {
-  return (
-    <div className="container mx-auto p-8">
-      <h1 className="text-3xl mb-8">Home Page (Timeline)</h1>
-    </div>
-  )
+  return redirect("/home")
 }
