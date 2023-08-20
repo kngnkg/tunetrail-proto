1553c61d760629237bc7ae7ee7c5fce49e678081
kngnkg <kawata.ryo.0518@gmail.com>
2023-08-20 09:04:47 +0000

    Fix: user page

--- webapp/app/(default)/[slug]/page.tsx
+++
@@ -1,8 +0,0 @@
-// ユーザーページ
-export default function UserPage({ params }: { params: { slug: string } }) {
-  return (
-    <div className="container mx-auto p-8">
-      <h1 className="text-3xl mb-8">User Page: {params.slug}</h1>
-    </div>
-  )
-}
