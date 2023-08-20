a5a65ee9cb9e77f06d93a709b18767e32957508d
kngnkg <kawata.ryo.0518@gmail.com>
2023-08-20 09:06:06 +0000

    Fix: getUser

--- webapp/services/user/get-user.ts
+++ webapp/services/user/get-user.ts
@@ -1,19 +1,34 @@
-import { FetchError, isApiError } from "@/types/error"
+import ApiError from "@/types/error"
 import { User, isUser } from "@/types/user"
 import fetcher from "@/lib/fetcher"
+
+const userNotFoundCode = 4201
 
 export const getUser = async (
   apiRoot: string,
   userName: string
-): Promise<User | FetchError> => {
-  const data = await fetcher(`${apiRoot}/user/${userName}`, {
-    method: "GET",
-    credentials: "include",
-  })
+): Promise<User | null> => {
+  try {
+    const data = await fetcher(`${apiRoot}/users/${userName}`, {
+      method: "GET",
+      credentials: "include",
+    })
 
-  if (!isUser(data)) {
-    throw new Error("Failed to fetch data from the API.")
+    if (!isUser(data)) {
+      throw new Error("Failed to fetch data from the API.")
+    }
+
+    return data
+  } catch (e) {
+    if (e instanceof ApiError) {
+      switch (e.code) {
+        case userNotFoundCode:
+          return null
+        default:
+          throw e
+      }
+    }
+
+    throw e
   }
-
-  return data
 }
