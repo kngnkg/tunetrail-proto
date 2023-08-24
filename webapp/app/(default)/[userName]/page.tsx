4d22c178ca1f8ad2b1d6a2c3aa28b7e3ff1ae121
kngnkg <kawata.ryo.0518@gmail.com>
2023-08-24 12:41:00 +0000

    Add: user page

--- webapp/app/(default)/[userName]/page.tsx
+++ webapp/app/(default)/[userName]/page.tsx
@@ -1,7 +1,11 @@
+import { cookies } from "next/headers"
+import Link from "next/link"
 import { notFound } from "next/navigation"
 import { getUser } from "@/services/user/get-user"
 
 import { env } from "@/env.mjs"
+import { Tokens } from "@/types/tokens"
+import { FollowButton } from "@/components/features/FollowButton/FollowButton"
 
 interface UserPageProps {
   params: { userName: string }
@@ -9,20 +13,45 @@ interface UserPageProps {
 
 // ユーザーページ
 export default async function UserPage({ params }: UserPageProps) {
-  const user = await getUser(env.API_ROOT, params.userName)
+  const cookieStore = cookies()
+  const idCookie = cookieStore.get("idToken")
+  const accessCookie = cookieStore.get("accessToken")
+
+  // これでいいか？
+  if (idCookie === undefined || accessCookie === undefined) {
+    // サインインページにリダイレクトしたほうがいいかも
+    notFound()
+  }
+
+  const tokens: Tokens = {
+    idToken: idCookie?.value,
+    accessToken: accessCookie?.value,
+    refreshToken: "",
+  }
+
+  const user = await getUser(env.API_ROOT, tokens, params.userName)
 
   if (!user) {
     notFound()
   }
 
+  // ログインユーザーの場合...
+
   return (
     <div className="container mx-auto p-8">
+      <Link href="/home">ホームへ</Link>
       <h1 className="text-3xl mb-8">User Page</h1>
+      <div>
+        {/* フォローボタンを押すと再レンダリングされない */}
+        <FollowButton userName={user.userName} isFollowing={user.isFollowing} />
+      </div>
       <div>
         <p className="text-xl mb-4">User Name: {user.userName}</p>
         <p className="text-xl mb-4">Name: {user.name}</p>
         <p className="text-xl mb-4">IconUrl: {user.iconUrl}</p>
         <p className="text-xl mb-4">Bio: {user.bio}</p>
+        <p className="text-xl mb-4">IsFollowing: {user.isFollowing}</p>
+        <p className="text-xl mb-4">IsFollowed: {user.isFollowed}</p>
         <p className="text-xl mb-4">Created: {user.createdAt}</p>
       </div>
     </div>
