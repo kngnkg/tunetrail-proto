4819dd61c739e2f1fdd8e4f5d5d5ea726e924d02
kngnkg <kawata.ryo.0518@gmail.com>
2023-09-04 12:44:25 +0000

    Update: package.json

--- webapp/app/(default)/home/page.tsx
+++ webapp/app/(default)/home/page.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link"
+
 import { PostList } from "@/components/features/PostList/PostList"
 
 // タイムライン
 export default function Home() {
   return (
     <div className="container mx-auto p-8">
-      <h1 className="text-3xl mb-8">Home Page (Timeline)</h1>
+      {/* <h1 className="text-3xl mb-8">Home</h1> */}
       <PostList />
     </div>
   )
